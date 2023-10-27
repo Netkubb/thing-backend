905815153db302eb72c9bcb2cee5e1529cfe9feb
Netkubb <net.chaosanguan@gmail.com>
2023-10-27 15:10:27 +0700

    connecting database ongoing
    and fix something that mig messing up

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-const feedRoutes = require("./feed/feed");
+const feedRoutes = require('./routes/feed');
+const auth = require('./routes/auth');
+const db = require('./database/db');
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use("/feed", feedRoutes);
+app.get('/testDB',async (req, res) => {
+    const result = await db.collection('user').get()
+    console.log(result.docs.map(doc => doc.data()))
+    res.send(1);
+});
+
+app.use('/feed', feedRoutes);
+
+app.use('/auth', auth);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ database/db.js
@@ -0,0 +1,8 @@
+const { initializeApp } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
+
+initializeApp();
+
+const db = getFirestore();
+
+module.exports = db;
