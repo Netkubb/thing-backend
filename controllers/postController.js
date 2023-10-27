f9427fc22caf2b201edacaa2c266fc1a134f2c44
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2023-10-27 15:04:25 +0700

    create postController

---
+++ controllers/postController.js
@@ -0,0 +1,55 @@
+//MOCK post
+
+const { GCProfiler } = require("v8");
+
+const getALLpost = async (req, res) => {
+  const post = []; // TODO Get all emplyee
+  // No content
+  if (!post) return res.status(204).json({ message: "No post found" });
+  res.json(post);
+};
+
+const createNewPost = async (req, res) => {
+  if (!req?.body?.videoURL) {
+    return res.status(400).json({ message: "No video link found" });
+  }
+
+  try {
+    // TODO create video ใน DB
+    const result = [];
+    res.status(201).json(result);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const updatePost = async (req, res) => {
+  if (!req?.body?.id) {
+    return res.status(400).json({ message: "ID are require" });
+  }
+
+  const post = []; // TODO find video in DB
+  if (!post)
+    return res
+      .status(400)
+      .json({ message: `Video ID ${req.body.id} not found` });
+
+  // UPDATE
+  if (req.body?.caption) post.caption = req.body.caption;
+  if (req.body?.like) post.caption = req.body.like;
+  const result = []; // TODO save post to DB
+  res.json(result);
+};
+
+const deletePost = async (req, res) => {
+  if (!req?.body?.id)
+    return res.status(400).json({ messgae: "post ID require" });
+  const post = []; // TODO find video in DB
+  if (!post)
+    return res
+      .status(400)
+      .json({ message: `Video ID ${req.body.id} not found` });
+
+  const result = []; // ลบ video
+  res.json(result);
+};
