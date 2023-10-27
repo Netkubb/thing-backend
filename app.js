babde36894861159d909e7d49cbbe62fa65d48eb
Netkubb <net.chaosanguan@gmail.com>
2023-10-27 14:42:55 +0700

    route auth

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const cors = require('cors');
 
-const feedRoutes = require('./feed/feed');
+const feedRoutes = require('./routes/feed');
+const auth = require('./routes/auth');
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 app.use('/feed', feedRoutes);
+
+app.use('/auth', auth);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
