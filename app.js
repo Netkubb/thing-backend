8cc406f25d05a5152ffd0ed7345c173adfd3c332
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2023-10-27 15:06:36 +0700

    merging mig ruin everything

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8000;
-const cors = require('cors');
 
-const feedRoutes = require('./routes/feed');
-const auth = require('./routes/auth');
+const feedRoutes = require("./feed/feed");
 
-app.use(express.json());
-app.use(cors());
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.use('/feed', feedRoutes);
-
-app.use('/auth', auth);
+app.use("/feed", feedRoutes);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
