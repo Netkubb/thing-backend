e5f6d3c88376069544e31048c7e3c6d65e69e3ee
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2023-10-29 13:07:51 +0700

    CREATE GET USER

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,20 @@
-/*const db = require("../database/db");
+const db = require("../database/db");
 
 const getUserProfile = async (req, res) => {
+  if (!req?.body?.token) return res.status(401).json({ message: "No token" });
+  const token = req.body.token;
+
   const query = await db.collection("user").get();
-  const users = query.docs.map((user) => user.data());
+  const foundUser = query.docs.find((user) => user.data().token === token);
 
-  const foundUser = users.find((user) => user.token === )
+  if (!foundUser) return res.status(400).json({ message: "No cookie" });
 
-};*/
+  const SendInfo = {
+    username: foundUser.data().username,
+    bulb: foundUser.data().bulb,
+  };
+
+  res.json(SendInfo);
+};
+
+module.exports = { getUserProfile };
--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authcontroller");
 const regController = require("../controllers/registerController");
+const userController = require("../controllers/userController");
 
 router.post("/login", authController.handlerLogin);
 router.post("/register", regController.handlerRegister);
+router.get("/me", userController.getUserProfile);
 
 module.exports = router;
